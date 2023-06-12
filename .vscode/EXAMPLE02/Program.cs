c5cb241a3305ef7236c5785a71f93996fff51c1c
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-13 00:02:42 +0300

    Приветствие для всех по имени

---
+++ .vscode/EXAMPLE02/Program.cs
@@ -0,0 +1,9 @@
+// Приветствие для пользователяё
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write($"Привет, {username} ");
+Console.Write(username);
+
+
+
