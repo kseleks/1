ea2d049283be625e050d2b00cb974f42ffe4b635
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-14 01:56:42 +0300

    принимаются 3 числа и вычисляется максимальное из них

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Напишите программу, которая принимает на вход три числа 
+// и выдаёт МАКСИМАЛЬНОЕ из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write ("ВВЕДИТЕ первое ЧИСЛО: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("ВВЕДИТЕ второе ЧИСЛО: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("ВВЕДИТЕ третье ЧИСЛО: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2 && number1>number3)
+{
+    Console.Write ($"Максимальное число {number1}  !");
+}
+else if (number2>number1 && number2>number3)
+{
+    Console.Write ($"Максимальное число {number2}");
+}
+else if (number3>number1 && number3>number2)
+{
+   Console.Write ($"Максимальное число {number3}");
+}
+else
+{
+    Console.Write ($"Есть повторяющиеся числа");
+    }
