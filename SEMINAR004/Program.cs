89b7c7c8d0e626cfaf3850b83ea9af29dffd15c1
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-14 00:36:48 +0300

    Вводим число и печатаем ряд от минус числа до плюс числа

---
+++ SEMINAR004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает одно число N, а на выходе показывает 
+// все целые числа в промежутке от -N до N
+
+// 4 --> -4,-3,-2,-1,0,1,2,3,4
+// 2 --> -2,-1,0,1,2
+
+Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО-");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int count = -number;
+
+while (number <= number)
+{
+    Console.Write($"{count}");
+    count ++;
+}
