6714015f0d9fcd5600bc48b5fa289cfbae784099
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-14 00:37:47 +0300

    Вводим трехзначную цифру, выводим остаток от деления на 10

---
+++ SEMINAR005/Program.cs
@@ -0,0 +1,13 @@
+﻿// // Написать программу, которая принимает на ВХОД трехзначное число 
+// и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine ("ВВЕДИТЕ трезначное число- ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number > 99 || number < 1000)
+{
+    Console.WriteLine (number = number % 10);
+}
+else
+{
+    Console.WriteLine ($"Число{number} не подходит по условию задачи");
+}
