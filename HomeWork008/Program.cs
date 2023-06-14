459a7b483be647840f03de7c696064c61e5ed579
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-14 16:06:32 +0300

    Программа выводит четные числа из числового ряда

--- HomeWork008/Program.cs
+++ HomeWork008/Program.cs
@@ -4,19 +4,34 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+// Console.Write ("Введите число- ");
+// int number = Convert.ToInt32 (Console.ReadLine());
+// int count = 1;
+
+// while (count <= number && number >0)
+//     { 
+//    if  (0 = count % 2)
+//     {
+//      Console.Write ($"{count}");
+//     }
+//     else count++;
+// }
+// { 
+//     Console.Write ($"Не подходит по условию задачи");
+//     return;
+// }
+
 Console.Write ("Введите число- ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32 (Console.ReadLine());
 int count = 1;
 
 while (count <= number)
-
-if  (count = count % 2)
 {
-    Console.Write ($"{count}");
-}
-else 
+    if (count % 2 == 0)
+    {
+        Console.Write ($"{count} ;");
+    }
 count++;
-
-{ 
-    Console.Write ($"Не подходит по условию задачи");
 }
+
+   
