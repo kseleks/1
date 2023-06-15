58f38c9558ebe75b691a1587afb9c570d67c18f0
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-16 02:14:00 +0300

    Первая задача на методы. DigitComparison

---
+++ SEMINAR006_digComparison/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу,которая выводит случайное число из отрезка [10,99]
+// и показывает НАИБОЛЬШУЮ цифру числа:
+// 78--- 8
+// 12---2
+// 85---8
+
+int DigitComparison (int num)
+{
+    int digit1 = num / 10;
+    int digit2 = num % 10;
+    int result = 0;
+    if (digit1 > digit2)
+    {
+        result=digit1;
+    }
+    else
+    {
+        result = digit2;
+    }
+return result;
+}
+int number = new Random().Next(10,100);
+System.Console.WriteLine($"Ваше случайное число: {number}");
+System.Console.WriteLine($"Максимальное целое число {number} равно {DigitComparison(number)}");
+
+
