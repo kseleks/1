478fb3760ec5903c74431b5a071e3d3837fde53e
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-20 02:10:22 +0300

    Программа которая проверяет делится ли число на 7 и 23 одновременно

--- SEMINAR014_OddEven7/Program.cs
+++ SEMINAR014_OddEven7/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+// 14 -- нет
+// 46 -- нет
+// 161 -- да
+
+bool  OddEven (int number)
+
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    { 
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+System.Console.WriteLine($"Введите число ");
+int numb = Convert.ToInt32 (Console.ReadLine());
+
+if (OddEven (numb))
+{
+    System.Console.WriteLine ($"Число {numb} кратно 7 & 23");
+}
+else
+{
+    System.Console.WriteLine ($"Число {numb} НЕ кратно 7 & 23");
+}
