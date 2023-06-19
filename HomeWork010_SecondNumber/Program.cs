8b4f6b91f2f19af4e002cb5606fec8b770f97c3c
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-20 01:12:39 +0300

    Gрограмма выводит вторую цифру трехзначного числа

---
+++ HomeWork010_SecondNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int SecondNumber (int argument)
+// {
+//     int arg1 = argument / 100;
+//     // промежуточный контейнер, определяем двухзначный остаток от числа, те 456 ---> 56
+//     // и потом выводим целочисленный остаток от деления 
+//     int argN = argument - arg1 * 100;
+//     int arg2 = argN / 10;
+
+//     return arg2;
+// }
+// int num = new Random().Next(100,1000);
+// System.Console.WriteLine($"Ваше случайное число {num}");
+// System.Console.WriteLine($"Вторая цифра числа равна - {SecondNumber(num)} ");
+
+
+
+// ДОРАБОТАННЫЙ СПОСОБ
+
+int SecondNumber (int argument)
+
+{
+int arg1 = argument / 100;
+int arg3 = argument % 10;
+int argN = argument - arg1 * 100 - arg3;
+int arg2 = argN /10;
+
+return arg2;
+}
+
+int num = new Random().Next(100,1000);
+System.Console.WriteLine($"Ваше случайное число {num}");
+System.Console.WriteLine($"Вторая цифра числа равна - {SecondNumber(num)} ");
+
+
+
+
+
---
+++ SEMINAR012_MultiplicityCheck/Program.cs
@@ -0,0 +1,32 @@
+﻿// напишите программу, которая будет принимать на вход два числа и выводить,
+// ЯВЛЯЕТСЯ ЛИ ВТОРОЕ ЧИСЛО КРАТНЫМ ПЕРВОМУ.
+
+// Если число 2 не кратно числу 1, то программа выводит ОСТАТОК от деления 
+
+// 34, 5 ---> не кратно ОСТАТОК от деления =4
+// 16, 4 ---> КРАТНО
+
+int MultiplicityCheck(int arg1, int arg2)
+{
+    int result = 0;
+    if (arg1/arg2 > 0)
+        { 
+            result = arg1/arg2;
+            return result;
+        }
+    else
+    {
+         return result;
+    }
+}
+
+int num1 = new Random().Next(0, 100);
+int num2 = new Random().Next(0,100);
+System.Console.WriteLine ($"Ваше случайное ПЕРВОЕ ЧИСЛО {num1}");
+System.Console.WriteLine ($"Ваше случайное ВТОРОЕ ЧИСЛО {num2}");
+
+
+
+
+// System.Console.WriteLine ($"ПЕРВОЕ ЧИСЛО {num1} кратно ВТОРОМУ ЧИСЛУ {num2}");
+System.Console.WriteLine ($"ПЕРВОЕ ЧИСЛО {num1} НЕ КРАТНО ВТОРОМУ ЧИСЛУ {num2}, остаток от деления равен {MultiplicityCheck(num1,num2)}");
