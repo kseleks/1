006961f14af53f671a7cdc962a27f1381dfeb2ca
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-29 00:46:40 +0300

    Программа по возведению числа А в степень числа Б

---
+++ HomeWork025_AB/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+double Exponentiation (double num1, double num2)
+{
+   double exponent =0;
+    if (num2==0)
+    {
+        exponent=1;
+        return exponent;
+    }
+    else
+    {
+       System.Console.WriteLine ($"{Math.Pow(num1,num2)}");
+        
+    }
+    return exponent= Math.Pow(num1,num2) ;
+
+}
+Console.WriteLine ("Введите 1 число:  ");
+int number1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 2 число:  ");
+int number2= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"Результат возведения в степень = {Exponentiation (number1, number2)}");
