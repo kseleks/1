0afc3a556e75263a0ffced6fb3ba214dd2e417b9
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-28 19:19:34 +0300

    произведение чисел от 1 до N

---
+++ SEMINAR028/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, 
+// которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int Proizvedenie (int num)
+{
+    int proizv =1;
+    for (int i =1; i<=num; i++ )
+    {
+        proizv*=i; 
+        // можно записать иначе (proizv+=i) то означает proizv=proizv+i
+    }
+    return proizv;
+}
+
+Console.WriteLine ("Введите число:  ");
+int number= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine ($"Произведение чисел от 1 до {number} = {Proizvedenie(number)}");
+
