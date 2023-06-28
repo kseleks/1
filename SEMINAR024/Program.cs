9c4ad0f86d08039374d44a342b61d24446fe5b40
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-06-28 19:06:30 +0300

    Программа по поиску суммы от 1 до N

---
+++ SEMINAR024/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Sum (int num)
+{
+    int sum =0;
+    for (int i =1; i<=num; i++ )
+    {
+        sum=sum+i; 
+        // ожно записать иначе (sum+=i) то означает sum=sum+i
+    }
+    return sum;
+}
+
+Console.WriteLine ("Введите число:  ");
+int number= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine ($"Сумма чисел от 1 до {number} = {Sum(number)}");
