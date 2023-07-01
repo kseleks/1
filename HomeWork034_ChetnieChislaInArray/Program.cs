2bde3ba997e34ad68b34c28f9f14efb5b27af779
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-07-01 09:04:39 +0300

    Программа находит количество четных чесел массива

---
+++ HomeWork034_ChetnieChislaInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int [] FillArrayWithRandomNumber(int size)
+// заполнение массива случайными числами
+{
+    int []arr=new int [size];
+    Random rnd=new Random();
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr[i]=rnd.Next(100,1000);
+    }
+    return arr;
+}
+
+System.Console.Write("ВВЕДИТЕ количество элементов массива: ");
+int length= Convert.ToInt32(Console.ReadLine());
+int [] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(String.Join(";", array));
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i]%2==0)
+{
+count++;
+}
+}
+System.Console.WriteLine(count);
+
