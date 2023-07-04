bea2477b2b26b400d739dd6db789654e791fbd4c
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-07-04 13:52:45 +0300

    Пргорамма перводит число из 10 с/с в 2-ую с/с

--- SEMINAR042/Program.cs
+++ SEMINAR042/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+
+void GetBinaryView(int num)
+{
+    // задаем точку остановки, иначе рекурсия будет бесконечной
+    if (num==0)
+    {
+        return;
+    }
+    GetBinaryView(num/2);
+    System.Console.Write(num%2 ); 
+}
+
+System.Console.Write("ВВЕДИТЕ ЧИСЛО десятичной системы исчисления: ");
+int number = Convert.ToInt32(Console.ReadLine());
+GetBinaryView(number);
+
+
+
+
+
+
+
+
