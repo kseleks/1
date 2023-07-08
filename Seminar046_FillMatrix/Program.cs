3ed4593fee6b04e2c9f517c7829e15fc3bec9a43
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-07-08 13:46:16 +0300

    Задаем двухмерный массив m*n с клавиатуры

--- Seminar046_FillMatrix/Program.cs
+++ Seminar046_FillMatrix/Program.cs
@@ -10,46 +10,76 @@
 // 77  3   8   1
 
 
-void FillMatrixWithRandom(int [,] matrix)
-// заполнить матрицу случайными числами
+// void FillMatrixWithRandom(int [,] matrix)
+// // заполнить матрицу случайными числами
+// {
+//     Random rnd=new Random();
+//     for (int i=0; i<matrix.GetLength(0); i++)
+//     {
+//         for(int j=0; j< matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j]=rnd.Next(0,10);
+//         }
+//     }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// // вывести на экран матрицу
+// {
+//     for (int i=0; i<matrix.GetLength(0); i++)
+//     {
+//         for(int j=0; j< matrix.GetLength(1); j++)
+//         {
+//             System.Console.WriteLine ($"{matrix[i,j]}");
+//         }
+//         System.Console.WriteLine ();
+//     // перенос на новую строку курсор
+//     }
+// }    
+
+
+// System.Console.WriteLine ("Введите количество строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine ("Введите количество столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+
+// // создаем двухмерный массив
+// int[,] matrix =new int [row,column];
+// FillMatrixWithRandom(matrix);
+// PrintMatrix(matrix);
+
+
+
+
+
+void FillMatrixWithRandom(int[,] matrix)
 {
-    Random rnd=new Random();
-    for (int i=0; i<matrix.GetLength(0); i++)
-    {
-        for(int j=0; j< matrix.GetLength(1); j++)
-        {
-            matrix[i,j]=rnd.Next(0,10);
-        }
-    }
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 10);
+}
+}
 }
 
 void PrintMatrix(int[,] matrix)
-// вывести на экран матрицу
 {
-    for (int i=0; i<matrix.GetLength(0); i++)
-    {
-        for(int j=0; j< matrix.GetLength(1); j++)
-        {
-            System.Console.WriteLine ($"{matrix[i,j]}");
-        }
-        System.Console.WriteLine ();
-    // перенос на новую строку курсор
-    }
-}    
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} ");
+}
+System.Console.WriteLine();
+}
+}
 
-
-System.Console.WriteLine ("Введите количество строк: ");
+System.Console.WriteLine("Введите кол-во строк: ");
 int row = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine ("Введите количество столбцов: ");
+System.Console.WriteLine("Введите кол-во столбцов: ");
 int column = Convert.ToInt32(Console.ReadLine());
-
-// создаем двухмерный массив
-int[,] matrix =new int [row,column];
+int[,] matrix = new int[row, column];
 FillMatrixWithRandom(matrix);
 PrintMatrix(matrix);
-
-
-
-
-
-
