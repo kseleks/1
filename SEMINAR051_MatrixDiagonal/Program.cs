525ccba0126325400609a5df0c89301602ea6831
Ксения <ksenia@MacBook-Pro-Ksenia.local>
2023-07-08 14:06:23 +0300

    Подсчет суммы всех элементов, которые находятся на главной диагонали

---
+++ SEMINAR051_MatrixDiagonal/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void FillMatrixWithRandom(int[,] matrix)
+// заполнение матрицы случайными числами
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 10);
+}
+}
+}
+
+void PrintMatrix(int[,] matrix)
+// вывод на экран матрицы
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} \t");
+}
+System.Console.WriteLine();
+}
+}
+
+int SumElementDiagonal(int[,] matrix)
+// подсчитываем сумму элементов матрицы если они находятся на главной диагонали
+{
+int sum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+if (i == j)
+{
+sum += matrix[i, j];
+// sum = sum+matrix[i, j];
+}
+}
+}
+return sum;
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[row, column];
+
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine(SumElementDiagonal(matrix));
